a942b46806762aae8ae87a08ea64bed316340b59
brytonpalmer <bpalmer2@byupathway.edu>
2026-07-05 21:42:45 -0600

    getting week 1 finished up

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const dns = require('node:dns/promises');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
+
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log('Database initialization error:', err);
+    }
+    else {
+        app.listen(port, () => {console.log(`Database is listening and node running on ${port}`)});
+    }
+}); 
+
 
